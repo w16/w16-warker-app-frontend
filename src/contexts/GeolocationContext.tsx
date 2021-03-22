c8e6e99df16609879b54c3c9104d89edaa398b57
Lucas Creator <lucascoelho@gmail.com>
2021-03-22 11:18:25 -0300

    Contextos finalizados

--- src/contexts/GeolocationContext.tsx
+++ src/contexts/GeolocationContext.tsx
@@ -1,6 +1,26 @@
-import { createContext, ReactNode } from "react";
+import React, { createContext, ReactNode, useEffect, useState } from "react";
+import * as Permissions from 'expo-permissions';
+import haversine from "haversine";
 
-interface GeolocationContextData {};
+/**
+ * Contexto de geolocalização. Expõe as seguintes propriedades:
+ * @const location -- coordenadas de localização do usuário ou do ponto inicial, caso não haja autorização para geolocalização.
+ * @func calculateDistance -- calcula distância de um dado ponto em relação à localização coom descrito acima com base na fórmula de haversine {@link https://en.wikipedia.org/wiki/Haversine_formula|Link} (distâncias entre dois pontos de uma esfera a partir de suas latitudes e longitudes).
+ */
+
+/* Localização inicial (home sWeet home) */
+const LATITUDE = -27.597;
+const LONGITUDE = -48.521;
+
+interface Coordinates {
+    latitude: number;
+    longitude: number;
+}
+
+interface GeolocationContextData {
+    location: Coordinates;
+    calculateDistance: (targetLocation: Coordinates) => number;
+};
 
 interface GeolocationProviderProps {
     children: ReactNode;
@@ -9,8 +29,59 @@ interface GeolocationProviderProps {
 export const GeolocationContext = createContext({} as GeolocationContextData);
 
 export function GeolocationProvider({children}: GeolocationProviderProps){
+    /* Atribuição da loclização inicial com uso do hook useState */
+    const [location, setLocation] = useState({
+        latitude: LATITUDE,
+        longitude: LONGITUDE
+    });
+
+    function calculateDistance(targetLocation: Coordinates):number {
+        return haversine(location, targetLocation) || 0;
+      };
+    
+    /** Solicita uma única vez, no primeiro acesso ao mapa
+     * @const permission -- permissão do usuário para acesso à geolocalização
+     */
+    const [permission, askForPermission] = Permissions.usePermissions(Permissions.LOCATION, {ask: true});
+            //askForPermission só funcionou nos testes em Android, sendo então abandonada.
+   
+    /** Hook useEffect utilizado para iniciar acompanhamento de geoposicionamento do usuário, recebendo como dependência @param permission como descrito acima.
+    */
+    useEffect(() => {
+        if (permission && permission.status === 'granted'){
+
+            /** 
+             * @const watchID -- retornada pelo navegador quando iniciado acompanhamento de geoposicionamento, é utilizada no retorno do hook para encerrar acompanhamento.
+             * @var position -- retorna coordenadas de geolocalização do usuário, armazenadas em @const location .
+             */
+            const watchID = navigator.geolocation.watchPosition(
+                position => {
+                    const { latitude, longitude } = position.coords;
+            
+                    const newCoordinate = {
+                        latitude,
+                        longitude
+                    };               
+                    
+                    setLocation(newCoordinate);
+                
+                },
+                error => console.log(error),
+                {
+                    enableHighAccuracy: true,
+                    timeout: 20000,
+                    maximumAge: 1000
+                }
+            )
+            return () => navigator.geolocation.clearWatch(watchID);
+        }
+    }, [permission]);
+
     return (
-        <GeolocationContext.Provider value={{}}>
+        <GeolocationContext.Provider value={{
+            location,
+            calculateDistance
+        }}>
             {children}
         </GeolocationContext.Provider>
     )
--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,6 +1,25 @@
-import { createContext, ReactNode } from "react";
+import React, { createContext, ReactNode, useState } from "react";
+import { register, login } from '../services/ApiService';
+import { t } from "i18n-js";
 
-interface UserContextData {};
+/**
+ * Contexto para tratamento de dados do usuário.
+ * Expõe as seguintes funções, que encaminham requisições diretamente à API {@link https://warker-api.herokuapp.com/ Warker}:
+ * @func handleRegister -- função de registro de usuário, que invoca em sequência a função de login, se não houver erros no registro.
+ * @func handleLogin -- realiza login do usuário, recuperando do servidor o nome fornecido e o token de acesso.
+ * @const token -- token de acesso fornecido pela API para consultas ao banco.
+ */
+
+/** interface para objeto de número variável de parâmetros associados aos dados de usuário */
+interface userData {
+    [key: string]: string;
+}
+
+interface UserContextData {
+    handleRegister: (data: userData) => Promise<boolean>;
+    handleLogin: (data: userData) => Promise<boolean>;
+    token: string;
+};
 
 interface UserProviderProps {
     children: ReactNode;
@@ -9,8 +28,61 @@ interface UserProviderProps {
 export const UserContext = createContext({} as UserContextData);
 
 export function UserProvider({children}: UserProviderProps){
+
+    const [token, setToken] = useState('');
+    const [userName, setUserName] = useState('');
+    const [carName, setCarName] = useState('');
+        // Apenas um pequeno mimo ao usuário.
+
+    /* Pequeno hack para utilizar a API para armazenar nome do usuário e nome do veículo numa só variável.*/
+    /** @func setNames -- atribui às @const userName e @const carName os valores armazenados na API no momento de registro. */
+    function setNames(user:string):void {
+        let userData = user.split('|');
+        setUserName(userData[0]);
+        let car = userData[1] ? userData[1] : t('carName');
+        setCarName(car);
+    }
+
+    const handleLogin = async (user: userData):Promise<boolean> => {
+        try{
+            const res = await login(user);
+            setToken(res.data.token);
+            setNames(res.data.user);
+            return true;
+        }catch(err){
+            console.log(err);
+            return false;
+        }
+    }
+
+    const handleRegister = async (data: userData):Promise<boolean> => {
+        try{
+            const user = {
+                /* Atributo name recebe nomes do usuário e do veículo */
+                "name": `${data.name.trim()}|${data.car.trim()}`,
+                "email": data.email,
+                "password": data.password
+            }
+            await register(user);
+            /* Se obtiver sucesso no registro, segue automaticamente para login; senão, erro é levantado e bloco try cancelado.
+            */
+            await handleLogin({
+                "email": data.email,
+                "password": data.password
+            })
+            return true;
+        }catch(err){
+            console.log(err);
+            return false;
+        }
+    }
+
     return (
-        <UserContext.Provider value={{}}>
+        <UserContext.Provider value={{
+            handleLogin,
+            handleRegister,
+            token
+        }}>
             {children}
         </UserContext.Provider>
     )
