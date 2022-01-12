34e98b3b14482761ab4f409c6dddf7d82bc167ef
renaanpio <renanps1@hotmail.com>
2022-01-11 22:57:38 -0300

    login screen stylized with SASS

--- warkerapp/src/pages/Login.tsx
+++ warkerapp/src/pages/Login.tsx
@@ -1,25 +1,26 @@
 import illustrationImg from "../assets/images/illustration.svg"
-import logoImg from "../assets/images/Warker.png"
+import logoImg from "../assets/images/gas.svg"
 import googleIconImg from "../assets/images/google-icon.svg"
+import "../styles/auth.scss"
 
 
 export function Login() {
     
     return(
-        <div>
+        <div id="page-auth">
             <aside>
                 <img src={illustrationImg}></img>
                 <strong>Fuel Check</strong>
                 <p>Search for the gas station closest to you in real time</p>
             </aside>
             <main>
-                <div>
+                <div className="main-content">
                     <img src={logoImg}></img>
-                    <button>
+                    <button className="sign-google" >
                         <img src={googleIconImg}></img>
                         Join w/ Google
                     </button>
-                    <div className="separator">join us</div>
+                    <div className="separator">rate us</div>
                 </div>
             </main>
         </div>
