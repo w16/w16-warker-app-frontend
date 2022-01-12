87c773778729ad3f6bf93417b98aa9eddb26ac6a
renaanpio <renanps1@hotmail.com>
2022-01-12 11:46:32 -0300

    add homepage structure

--- warkerapp/src/App.tsx
+++ warkerapp/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 // import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Login></Login>
+      <Home></Home>
     </div>
   );
 }
---
+++ warkerapp/src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import illustrationImg from "../assets/images/illustration.svg"
+import "../styles/auth.scss"
+
+export function Home() {
+    return(
+
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg}></img>
+                <strong>Fuel Check</strong>
+                <p>Search for the gas station closest to you in real time</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                   <h1>APP</h1>
+                </div>
+            </main>
+        </div>
+
+
+
+
+
+    )
+}
